9ea1e07c8f4dfbea17a116514e2a488df13f2c88
BobLynn <476127332@qq.com>
2019-06-28 15:32:01 +0800

    addtuple_and_list_ex.py

---
+++ tuple_and_list_ex.py
@@ -0,0 +1,8 @@
+a_tuple = (3,1,4,1,5,9)
+a_list = [3,1,4,1,5,9]
+
+
+print(a_tuple)
+print("--------")
+print("run 'list' for the first time.",a_list)
+print("run 'list' for the second time.",a_list)
