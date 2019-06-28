c1d37a3d6bb3692e17ef07662189b5df883eeb90
BobLynn <476127332@qq.com>
2019-06-28 11:17:46 +0800

    add list_ex.py

---
+++ list_ex.py
@@ -0,0 +1,19 @@
+a = [1,1,4,5,1,4]
+
+a.append(0)     #append a number 0
+print(a)        #a = [1,1,4,5,1,4,0]
+
+a = [1,1,4,5,1,4]
+a.insert(1,0)   #append a number 0 after 1
+print(a)        #a = [1,0,1,4,5,1,4]
+
+a = [1,1,4,5,1,4]
+a.remove(1)     #remove 1
+print(a)
+print("")
+a = [1,1,4,5,1,4]
+print(a[1])     #1
+print(a[-1])    #4
+print(a[0:3])   #[1,1,4]
+print(a[5:])    #[4]
+print(a[-3:])   #[5,1,4]
