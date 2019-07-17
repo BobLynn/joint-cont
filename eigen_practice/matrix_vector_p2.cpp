756df2528aefb2aa2f9e8468758e058bb105e8f9
BobLynn <476127332@qq.com>
2019-07-17 17:37:42 +0800

    add matrix_vector_p2.cpp

---
+++ eigen_practice/matrix_vector_p2.cpp
@@ -0,0 +1,32 @@
+//----Matrix and vector arithmetic Part2----
+#include <iostream>
+#include <Eigen/Dense>
+
+
+
+//----矩陣、向量的乘除（乘以小於一的數）----
+using namespace Eigen;
+
+int main ()
+{
+    Matrix2d a;
+    a << 1, 2,
+         3, 4;
+
+    Vector3d v(1,2,3);
+    std::cout << "a * 2.5 =\n" << a * 2.5 << std::endl;
+    std::cout << "0.1 * v =\n" << 0.1 * v << std::endl;
+    std::cout << "Doing v *= 2;" << std::endl;
+    v *=2;
+    std::cout << "Now v = \n" << v << std::endl;
+
+
+
+//----矩陣的轉置、共軛矩陣、餘因子矩陣----
+    MatrixXcf b = MatrixXcf::Random(2,2);
+    cout << "Here is the matrix b\n" << b << endl;
+    cout << "Here is the matrix b^T\n" << b.transpose() << endl;
+    cout << "Here is the conjugate of b\n" << b.conjugate() << endl;
+    cout << "Here is the matrix b^*\n" << b.adjoint() << endl;
+
+    }
